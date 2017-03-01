4ce2e5173a733ebb0714fd544af3846fa15e571b
bhavinafricawala <bhavinafricawala@gmail.com>
2017-02-28 19:55:00 -0800

    Starting Use Factory Pattern with Simple Injector

---
+++ AdventureWorks.Factory/ProductFactory.cs
@@ -0,0 +1,30 @@
+﻿using AdventureWorks.Business.Product;
+using AdventureWorks.Data.Interfaces;
+using AdventureWorks.Data.ProductData;
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorks.Factory
+{
+    public static class ProductFactory
+    {
+        private static Container container = new Container();
+
+        static ProductFactory()
+        {
+            container.Register<IRepository<Product>, ProductAccess>(Lifestyle.Singleton);
+            
+        }
+
+        public static IRepository<Product> GetProductAccess()
+        {
+            IRepository<Product> product=(ProductAccess)container.GetInstance(typeof(IRepository<Product>));
+
+            return product;
+        }
+    }
+}
